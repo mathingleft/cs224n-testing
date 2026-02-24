99192f07f60b20cf00b798a3eef16b8f953ecfdc
Sophia Li <sophia.li.g4c2@gmail.com>
2026-02-23 23:06:47 -0800

    blah

--- testyyyyy/vanilla-sdft/train.py
+++ testyyyyy/vanilla-sdft/train.py
@@ -16,6 +16,7 @@ def sdft():
     import json 
     import math
     import torch
+    import torch.nn.functional as F
     qwen_model = "/vol/models/base"
     tokenizer = AutoTokenizer.from_pretrained(qwen_model)
     student = AutoModelForCausalLM.from_pretrained(qwen_model, device_map="auto", torch_dtype="auto")
@@ -25,32 +26,52 @@ def sdft():
     data = json.load(open("/vol/data/gemini_verified_proofs.json", "r"))
     random.shuffle(data)
     batch_size = math.ceil(len(data) / 5)
+    max_new_tokens = 2048
+    temperature = 1.0
+    optimizer = torch.optim.AdamW(student.parameters(), lr=1e-5) 
     for i in range(5):
         batch = data[i*batch_size:(i+1)*batch_size]
         for entry in batch: 
             input = tokenizer(entry["formal_statement"], return_tensors="pt", padding=True).to(student.device)
+            length = len(input["input_ids"][0])
+            all_logits = []
             teacher_prompt = f"""
                 Reference proof: {entry["formal_proof"]}
 
                 {entry["formal_statement"]}
             """
+            for step in range(max_new_tokens):
+                outputs = student(input)
+                next_token_logits = outputs.logits[:, -1, :]
+                all_logits.append(next_token_logits)
+
+                probs = torch.softmax(next_token_logits/temperature, dim=-1)
+                next_token = torch.multinomial(probs
+                                               , num_samples=1)
+                
+                input_ids = torch.cat([input["input_ids"], next_token], dim=-1)
+                if (next_token.item() == tokenizer.eos_token_id):
+                    break
+
+            student_logits = torch.stack(all_logits)
+            generated_tokens = input_ids[:, length: ]
             teacher_input = tokenizer(teacher_prompt, return_tensors="pt", padding=True).to(teacher.device)
             with torch.no_grad():
-                output = student.generate(**input, max_new_tokens=2048)
-                teacher_output = teacher.generate(**teacher_input, max_new_tokens=2048)
+                teacher_outputs = teacher(**teacher_input)
+            
+            T = tokenizer(teacher_prompt, return_tensors="pt").input_ids.shape[-1]
 
-    # from huggingface_hub import snapshot_download
-    # from datasets import load_dataset
-    # import os           
-    # os.makedirs("/vol/data", exist_ok=True)
-    # dataset = load_dataset("AI-MO/NuminaMath-LEAN", split="train")
-    # # dataset = dataset.to_dict()
-    # data = []
-    # for entry in dataset:
-    #     if ((entry["formal_proof"]) != None and len(entry["formal_proof"]) > 0):
-    #         data.append(entry)
-    # import json                                                                                                    
-    # with open("/vol/data/gemini_verified_proofs.json", "w") as f:                                                  
-    #     json.dump(data, f, indent=2)  
-    # snapshot_download(repo_id="Qwen/Qwen2.5-3B", local_dir="/vol/models/base")
-    # vol.commit()
+            N = generated_tokens.shape[-1]
+
+            teacher_logits = teacher_outputs.logits[:, T:T+N, :]
+
+            #CALCULATE LOB PROBSD 
+
+            student_logs = F.log_softmax(student_logits, dim=-1)
+            teacher_logs = F.log_softmax(teacher_logits, dim=-1)
+
+            loss = F.kl_div(student_logs, teacher_logs, log_target=True, reduction="batchmean")
+            loss.backwards()
+            optimizer.step()
+            optimizer.zero_grad()
+
