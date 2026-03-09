9fe35c32c140db4c45a9c283c167e66ae5c41f16
Jack Li <jack@Jack-Thinkpad-Debian>
2026-03-09 14:57:02 -0700

    updated compare.py's file path to Numina_good_last40.json

--- testyyyyy/model-testing/compare.py
+++ testyyyyy/model-testing/compare.py
@@ -14,7 +14,7 @@ TEMPERATURE   = 0.9
 MAX_NEW_TOKENS = 32768
 DATA_FORMAT   = "theorem"
 COLUMN        = "formal_statement"
-VOLUME_FILE   = "Numina_bad_50.json"
+VOLUME_FILE   = "Numina_good_last40.json"
 RANDOM_SEED   = 42
 
 PREAMBLE = (
